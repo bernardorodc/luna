77e492e0b5d5a02afb79091a6bd038293a7a91d2
bernardorodc <bernardorodc@gmail.com>
2025-06-10 09:08:05 -0400

    fix: Adiciona base path para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/luna/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
@@ -12,3 +13,5 @@ export default defineConfig({
     },
   },
 })
+
+
